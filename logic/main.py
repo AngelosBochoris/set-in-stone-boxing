dffe65cecf8caa27966c969195c4ceed61500a56
dk729@cam.ac.uk <kvit2006@gmail.com>
2026-02-28 14:39:02 +0000

    Processing of moves added

--- logic/main.py
+++ logic/main.py
@@ -9,21 +9,33 @@ class Player:
         # TODO
 
     pass
-class Game:
-    action_matrix: Dict[Tuple[Move, Move], Any] = get_action_matrix()
 
-    def __init__(self, p1_id: int, p2_id: int, inform_state_update: Callable[[Dict[int, Player]], None]):
+
+class GameState:
+    action_matrix: Dict[Tuple[Move, Move], Tuple] = get_action_matrix()
+
+    def __init__(self, p1_id: int, p2_id: int, inform_state_update: Callable[[Dict[int,Any]], None]):
         self.players = {p1_id: Player(p1_id), p2_id: Player(p2_id)}
         self.inform_state_update = inform_state_update
 
-    def play_move(self, p1_move: Move, p2_move: Move):
+    def _play_move(self, p1:int, p1_move: Move, p2:int, p2_move: Move) -> None:
         actions = self.action_matrix[p1_move, p2_move]
-        # TODO: play actions
+        self.inform_state_update({p1:actions[0], p2:actions[1]})
 
-        self.inform_state_update(self.players)
+    def play_moves(self, players_moves: Dict[int, List[Move]]) -> None:
+        assert len(players_moves) == 2
+        with len(next(iter(players_moves.items()))) as l:
+            assert all([len(players_moves[i]) == l for i in players_moves])
+        p1, p2 = [i for i in players_moves.keys()]
+
+        for p1_move, p2_move in zip(players_moves[p1], players_moves[p2]):
+            self._play_move(p1, p1_move, p2, p2_move)
+
+    def process_my_moves(self, p_id: int, p_self_moves: List[Move]):
+        x = []
+        # --- TODO: call network function for exchange
+        # x = ...
         pass
-
-    def play_moves(self, p1_moves: List[Move], p2_moves: List[Move]) -> None:
-        assert len(p1_moves) == len(p2_moves)
-        for p1_move, p2_move in zip(p1_moves, p2_moves):
-            self.play_move(p1_move, p2_move)
+        # ----
+        moves = {i: p_self_moves if i == p_id else x for i in self.players}
+        self.play_moves(moves)
--- logic/utils.py
+++ logic/utils.py
@@ -1,6 +1,7 @@
 import json
 from enum import Enum, auto
 
+
 class Move(str, Enum):
     ATT_L = "ATT_L"
     ATT_R = "ATT_R"
@@ -9,14 +10,16 @@ class Move(str, Enum):
     COU_L = "COU_L"
     COU_R = "COU_R"
 
+
 moves = [move.value for move in Move]
+
+
 def get_action_matrix():
     with open('data.json', 'r') as file:
         data = json.load(file)
-    ans = {(i,j):None for i in moves for j in moves}
+    ans = {(i, j): None for i in moves for j in moves}
     for i in moves:
         for j in moves[moves.index(i):]:
             result = data[i][j]
             # TODO
             pass
-
