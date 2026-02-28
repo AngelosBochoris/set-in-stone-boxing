9bc783d0536bce5e853a9c88f77a92ed6dce6ffb
dk729@cam.ac.uk <kvit2006@gmail.com>
2026-02-28 21:03:24 +0000

    small refractor

--- controller/network/server.py
+++ controller/network/server.py
@@ -1,5 +1,7 @@
 import socket
 import random
+
+
 class host():
     def __init__(self):
         #info about host
@@ -14,18 +16,18 @@ class host():
         self.s.bind((self.name, self.port))
         # lets 2 ppl connect
         self.s.listen(2)
-        self.moves=random.randint(2,6)
+        self.moves = random.randint(2, 6)
 
     def start(self):
-        #stack with all the clients
-        self.clients=[]
+        # stack with all the clients
+        self.clients = []
         print("Server is listening")
 
-        while len(self.clients)<2:
+        while len(self.clients) < 2:
             clientSocket, addr = self.s.accept()
             clientSocket.send(bytes(str(len(self.clients)), "utf-8"))
-            print("Client connected from",len(self.clients))
-            self.clients.append((clientSocket,addr))
+            print("Client connected from", len(self.clients))
+            self.clients.append((clientSocket, addr))
 
         print("enough players to start game")
 
@@ -34,25 +36,25 @@ class host():
             soc.send(bytes("Game_started", "utf-8"))
             soc.send(bytes(f"Game_started {self.moves}", "utf-8"))
         self.game_over = False
-        self.data = ["0","0"]
+        self.data = ["0", "0"]
 
         #game playing
         while not self.game_over:
             #tries to recive the client data
             for i in range(len(self.clients)):
                 soc, addr = self.clients[i]
-                message=soc.recv(1024).decode("utf-8")
-                if message=='' or message=="End":
+                message = soc.recv(1024).decode("utf-8")
+                if message == '' or message == "End":
                     soc.close()
                     self.clients.remove((soc, addr))
                     self.game_over = True
                     break
                 else:
-                    self.data[i]=message
+                    self.data[i] = message
 
             #sends stuff back to the client
-            for soc, addr  in self.clients:
-                if(self.game_over):
+            for soc, addr in self.clients:
+                if (self.game_over):
                     break
                 soc.send(bytes("£@€".join(self.data), "utf-8"))
         #game is done
@@ -62,5 +64,3 @@ class host():
             self.clients.remove((soc, addr))
         self.s.close()
         print("Host quit")
-
-
